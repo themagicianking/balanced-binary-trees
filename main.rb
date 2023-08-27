63ba5aaa6524ed6920a281b8812cc7a086443c62
themagicianking <tmoonder@gmail.com>
2023-08-27 17:53:28 -0400

    Initial setup.

---
+++ main.rb
@@ -0,0 +1,14 @@
+class Node
+  attr_accessor :head, :left, :right
+end
+
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    @array = array
+  end
+
+  def build_tree(array)
+  end
+end
