56b03970b660001d54f388ba87bd797ffd5f3afa
themagicianking <tmoonder@gmail.com>
2023-08-27 17:59:33 -0400

    Initial setup continued.

--- main.rb
+++ main.rb
@@ -11,4 +11,43 @@ class Tree
 
   def build_tree(array)
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value)
+  end
+
+  def delete(value)
+  end
+
+  def find(value)
+  end
+
+  def level_order(block)
+  end
+
+  def inorder(block)
+  end
+
+  def preorder(block)
+  end
+
+  def postorder(block)
+  end
+
+  def height(node)
+  end
+
+  def depth(node)
+  end
+
+  def balanced?
+  end
+
+  def rebalance
+  end
 end
