bb919e63f72e345af7a09cc69a811647482fa697
themagicianking <tmoonder@gmail.com>
2024-01-17 14:43:53 -0500

    Returning to this after some time away and saving the current progress before scrapping it to start fresh.

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 class Node
-  attr_accessor :head, :left, :right
+  include Comparable
+
+  attr_accessor :value, :left, :right
 end
 
 class Tree
@@ -9,7 +11,31 @@ class Tree
     @array = array
   end
 
-  def build_tree(array)
+  def build_tree
+    if array.nil?
+      array = @array.sort.uniq
+    end
+    mid = array[(array.length)/2.round]
+    left = array[0..((array.length/2.round) - 1)]
+    right = array[((array.length/2.round) + 1)..-1]
+    if node.nil?
+      @root = Node.new
+      node = @root
+    end
+    array = left
+    until array.length < 3
+      node.value = mid
+      node.left = Node.new
+      node.right = Node.new
+      array = left
+      build_tree
+      # etc etc
+    end
+    #array = right
+    #node = @root
+    #until 
+      # hmm.
+    #end
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -51,3 +77,7 @@ class Tree
   def rebalance
   end
 end
+
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 8, 67, 6345, 324])
+
+tree.build_tree
